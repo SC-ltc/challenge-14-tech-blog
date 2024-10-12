4cdca905a37ee631d0642161ba4f250a395d7ecc
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-10-12 15:41:59 -0700

    added initial files for the controller folder

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const {Post} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Create a new blog post
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+// Update a blog post by its ID
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const updatePost = await Post.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!updatePost) {
+        res.status(404).json({ message: 'No blog post found with that id' });
+        return;
+      }
+      res.status(200).json(updatePost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });

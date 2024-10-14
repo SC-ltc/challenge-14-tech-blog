bdd414893e99e62e7f43e47ab49a0b46aa4f80b7
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-10-14 15:13:44 -0700

    completed the routes under the api folder in controllers

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -33,3 +33,25 @@ router.put('/:id', withAuth, async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // Delete a blog post by its ID
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const deletePost = await Project.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!deletePost) {
+        res.status(404).json({ message: 'No blog post found with that id' });
+        return;
+      }
+  
+      res.status(200).json(deletePost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
